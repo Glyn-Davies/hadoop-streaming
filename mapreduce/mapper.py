cb3757303451b2cc1f8e4f91b43c9c882aa9b851
Glyn-Davies <34030131+Glyn-Davies@users.noreply.github.com>
2018-11-21 20:14:59 +0000

    Add files via upload

--- mapreduce/mapper.py
+++ mapreduce/mapper.py
@@ -1,17 +1,13 @@
 #!/ u s r / bin / env python
 ”””mapper.py ”””
 import sys
-# i n p u t comes from STDIN ( s t and a rd i n p u t )
+
 for line in sys.stdin:
-# remove l e a di n g and t r a i l i n g whi te s p ace
-line = line . strip( )
-# s p l i t the l i n e i n t o words
-words = line.split()
-# i n c r e a s e c o u n t e r s
-for word in words :
-# w ri t e the r e s u l t s t o STDOUT ( s t and a rd output ) ;
-# what we output h e r e w i l l be the i n p u t f o r the
-# Reduce s tep , i . e . the i n p u t f o r r e d u c e r . py
-#
-# tab−d elimi t e d ; the t r i v i a l word count i s 1
-print ’%s\t%s’ % (word, 1)
+
+    line = line . strip()
+
+    words = line.split()
+
+    for word in words :
+
+        print ’%s\t%s’ % (word, 1)
